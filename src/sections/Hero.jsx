8af0d47c9e643a24f4418ff287be74a3e2058f55
inbumbaek <inbumbaek@gmail.com>
2023-10-25 10:41:33 -0700

    hero

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -1,6 +1,11 @@
 const Hero = () => {
   return (
-    <div>Hero</div>
+    <section
+      id="home"
+      className="w-full flex xl:flex-row flex-col justify-center min-h-screen border-2 border-red-500 p-2"
+      >
+
+    </section>
   )
 }
 
