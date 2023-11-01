2b2237eb4f7391004e0c49e893ccfb67412f6df5
inbumbaek <inbumbaek@gmail.com>
2023-11-01 15:58:53 -0700

    add image

--- src/sections/SpecialOffer.jsx
+++ src/sections/SpecialOffer.jsx
@@ -1,8 +1,8 @@
 const SpecialOffer = () => {
   return (
     <section className="flex justify-wrap items-center max-xl:flex-col-reverse gap-10 max-container">
-      <div>
-        
+      <div className="flex-1">
+        <img src={offer} width={773} height={687} className="object-contain w-full" />
       </div>
     </section>
   )
