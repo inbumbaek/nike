6335d7b0b6846014885bde457bc4d66dd025dbe2
inbumbaek <inbumbaek@gmail.com>
2023-11-09 14:26:41 -0800

    working on reviewcard

--- src/components/ReviewCard.jsx
+++ src/components/ReviewCard.jsx
@@ -1,6 +1,24 @@
-const ReviewCard = () => {
+import { star } from "../assets/icons";
+
+const ReviewCard = ({ imgURL, customerName, rating, feedback }) => {
   return (
-    <div>ReviewCard</div>
+    <div className="flex justify-center items-center flex-col">
+      <img
+        src={imgURL}
+        alt="customer"
+        className="rounded-full object-cover w-[120px] h-[120px]"
+      />
+      <p>{feedback}</p>
+      <div>
+        <img
+          src={star}
+          width={24}
+          height={24}
+          className="object-contain m-0"
+        />
+        <p>({rating})</p>
+      </div>
+    </div>
   )
 }
 
