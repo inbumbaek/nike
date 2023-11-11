e886c7780aa5ff3694fc6a9429bd3ac277594728
inbumbaek <inbumbaek@gmail.com>
2023-11-10 23:11:52 -0800

    add color

--- src/sections/Subscribe.jsx
+++ src/sections/Subscribe.jsx
@@ -2,8 +2,8 @@ const Subscribe = () => {
   return (
     <section className="max-container flex justify-between items-center max-lg:flex-col gap-10"
     id="contact-us">
-      <h3>Sign Up from
-        <span>
+      <h3 className="text-4xl leading-[68px] lg:max-w-md font-palanquin font-bold">Sign Up from
+        <span className="text-coral-red">
           Updates
         </span> & Newsletter
       </h3>
