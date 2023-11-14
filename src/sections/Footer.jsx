be8d9dc130734a2c82d64a3d2217866cf7614f4e
inbumbaek <inbumbaek@gmail.com>
2023-11-14 02:26:20 -0800

    add section

--- src/sections/Footer.jsx
+++ src/sections/Footer.jsx
@@ -21,6 +21,16 @@ const Footer = () => {
             ))}
           </div>
         </div>
+
+        <div className='flex flex-1 justify-between lg:gap-10 gap-20 flex-wrap'>
+          {footerLinks.map((section) => (
+            <div key={section.title}>
+              <h4 className='font-montserrat text-2xl leading-normal font-medium mb-6 text-white'>
+                {section.title}
+              </h4>
+              </div>
+              ))}
+          </div>
       </div>
     </footer>
   );
