5df62bb33e57fd50f0f94305496cc39919df27f2
inbumbaek <inbumbaek@gmail.com>
2023-11-13 16:04:56 -0800

    add p tag

--- src/sections/Footer.jsx
+++ src/sections/Footer.jsx
@@ -4,8 +4,12 @@ const Footer = () => {
       <div className="flex justify-between items-start gap-20 flex-wrap max-lg:flex-col">
         <div className="flex flex-col items-start">
           <a href="/">
-            <img src={footerLogo} width={150} height={46} />
+            <img src={footerLogo} width={150} height={46} className='m-0'/>
           </a>
+          <p className='mt-6 text-base leading-7 font-montserrat text-white-400 sm:max-w-sm'>
+            Get shoes ready for the new term at your nearest Nike store. Find
+            Your perfect Size In Store. Get Rewards
+          </p>
         </div>
       </div>
     </footer>
