7d2fdfa41b12b629abd371e6e69943f8e3c0b8e5
inbumbaek <inbumbaek@gmail.com>
2023-11-14 02:53:38 -0800

    add section link

--- src/sections/Footer.jsx
+++ src/sections/Footer.jsx
@@ -28,6 +28,16 @@ const Footer = () => {
               <h4 className='font-montserrat text-2xl leading-normal font-medium mb-6 text-white'>
                 {section.title}
               </h4>
+              <ul>
+                {section.links.map((link) => (
+                  <li
+                    className='mt-3 font-montserrat text-base leading-normal text-white-400 hover:text-slate-gray'
+                    key={link.name}
+                  >
+                    <a href={link.link}>{link.name}</a>
+                  </li>
+                ))}
+              </ul>
               </div>
               ))}
           </div>
